d25052930e01d5d625e12fc9f04e85aa8f4a8f17
kamigokun <rawatharshit8383@gmail.com>
2026-04-23 01:15:37 +0530

    feat: add show_usage helper function

--- task_cli/main.py
+++ task_cli/main.py
@@ -1,5 +1,5 @@
 import sys
-from task_cli.commans import(
+from task_cli.commands import(
     add_task,
     list_tasks,
     delete_task,
@@ -7,3 +7,21 @@ from task_cli.commans import(
     mark_task
 )
 
+
+def show_usage():
+    # This prints instructions when user types wrong command
+    # or doesn't type anything at all
+    print("""
+Usage: python main.py <command> [arguments]
+
+Commands:
+  add <description>              Add a new task
+  list                           List all tasks
+  list done                      List completed tasks
+  list todo                      List pending tasks
+  list in-progress               List in-progress tasks
+  update <id> <description>      Update a task
+  delete <id>                    Delete a task
+  mark-done <id>                 Mark task as done
+  mark-in-progress <id>          Mark task as in progress
+    """) 
